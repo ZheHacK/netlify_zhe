0325edc927239ab279250df0251c88ee63e402c1
ZheHacK <54292567+ZheHacK@users.noreply.github.com>
2023-07-19 21:26:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Netlify with Node.js!');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Netlify with Node.js!');
 });
 
 const port = process.env.PORT || 3000;
